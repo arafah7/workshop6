e50fd57c02bc1889d16cacd76a8e44c516c326fb
arafah7 <163745344+arafah7@users.noreply.github.com>
2025-04-08 20:08:20 +0930

    Update wizard.cpp

--- wizard.cpp
+++ wizard.cpp
@@ -12,8 +12,8 @@ Wizard::Wizard() : Player() {
 }
 
 void Wizard::castSpell(Player* opponent) {
-      cout << get_name << " casts a spell on " << opponent->get_name() << " for " << get_damage << " damage.\n";
-    opponent->takeDamage(get_damage);
+      cout << get_name() << " casts a spell on " << opponent->get_name() << " for " << get_damage() << " damage.\n";
+    opponent->takeDamage(get_damage());
 }
 
 
